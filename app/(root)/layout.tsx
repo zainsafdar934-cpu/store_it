cd5c9fe5f5bc9628461feed92d51cfbd482f2b56
Muhammad Zain <zainsafdar934@gmail.com>
2026-06-24 22:51:55 +0500

    add sidebar, mobile navbar and fetching current user detail

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/Header";
+import MobileNavigation from "@/components/MobileNavigation";
+import Sidebar from "@/components/Sidebar";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
+  return (
+    <main className="flex h-screen">
+      <Sidebar {...currentUser} />
+      <section className="flex h-full flex-1 flex-col">
+        <MobileNavigation {...currentUser} />
+        <Header />
+        <div className="main-content">{children}</div>
+      </section>
+    </main>
+  );
+};
+
+export default Layout;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 const getUserByEmail = async (email: string) => {
   const { database } = await createAdminClient();
@@ -57,8 +58,7 @@ export const createAccount = async ({
       {
         fullName,
         email,
-        avatar:
-          "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
+        avatar: avatarPlaceholderUrl,
         accountId,
       },
     );
@@ -90,4 +90,20 @@ export const verifySecret = async ({
   } catch (error) {
     handleError(error, "Failed to verify OTP");
   }
+};
+
+export const getCurrentUser = async () => {
+  const { database, account } = await createSessionClient();
+
+  const result = await account.get();
+
+  const user = await database.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("accountId", [result.$id])],
+  );
+
+  if (user.total <= 0) return null;
+
+  return parseStringify(user.documents[0]);
 };
