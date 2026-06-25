371e9ba0a608feb6b61e8364f9be6cfc067c895d
Muhammad Zain <zainsafdar934@gmail.com>
2026-06-25 17:07:47 +0500

    add uploading files functionality

---
+++ lib/actions/file.actions.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { InputFile } from "node-appwrite/file";
+import { createAdminClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
+import { ID } from "node-appwrite";
+import { constructFileUrl, getFileType, parseStringify } from "../utils";
+import { revalidatePath } from "next/cache";
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+export const uploadFile = async ({
+  file,
+  ownerId,
+  accountId,
+  path,
+}: UploadFileProps) => {
+  const { storage, database } = await createAdminClient();
+
+  try {
+    const inputFile = InputFile.fromBuffer(file, file.name);
+
+    const bucketFile = await storage.createFile(
+      appwriteConfig.bucketId,
+      ID.unique(),
+      inputFile,
+    );
+
+    const fileDocument = {
+      type: getFileType(bucketFile.name).type,
+      name: bucketFile.name,
+      url: constructFileUrl(bucketFile.$id),
+      extension: getFileType(bucketFile.name).extension,
+      size: bucketFile.sizeOriginal,
+      users: [],
+      ownerId,
+      accountId,
+      bucketFileId: bucketFile.$id,
+    };
+
+    const newFile = await database
+      .createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.filesCollectionId,
+        ID.unique(),
+        fileDocument,
+      )
+      .catch(async (error: unknown) => {
+        await storage.deleteFile(appwriteConfig.bucketId, bucketFile.$id);
+        handleError(error, "Failed to create file document");
+      });
+
+    revalidatePath(path);
+    return parseStringify(newFile);
+  } catch (error) {
+    handleError(error, "Failed to upload file");
+  }
+};
